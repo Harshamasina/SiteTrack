25bcb261690cfd346031bdcaa6b699f882ed24e1
Harshamasina <harshamasina1999@gmail.com>
2026-01-15 13:11:16 -0600

    changed: "env file"

--- app/_components/AppHeader.tsx
+++ app/_components/AppHeader.tsx
@@ -5,7 +5,6 @@ import React from 'react'
 function AppHeader() {
     return (
         <div className='p-4 shadow-sm flex items-center justify-between w-full '>
-            <SidebarTrigger />
 
         </div>
     )
