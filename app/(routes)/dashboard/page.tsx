6fa0f153aa979e879efe30e71aac37638dffed5e
Harshamasina <harshamasina1999@gmail.com>
2026-01-15 16:23:20 -0600

    added: "add website input"

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,11 +1,31 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useState } from "react";
+import addImage from "../../../public/www.png";
 
 const Dashboard = () => {
+    const [websiteList, setWebsiteList] = useState([]);
     return (
         <div className="mt-8">
             <div className="flex justify-between items-center">
                 <h2 className="font-bold text-xl">My Website</h2>
                 <Button>+ Website</Button>
+            </div>
+
+            <div>
+                {
+                    websiteList?.length == 0 ? 
+                        <div className="flex flex-col justify-center items-center gap-4 p-8 border-2 border-dashed rounded-2xl mt-5">
+                            <Image src={addImage} alt="add website" width={100} height={100}/>
+                            <h2>You don't have any website added for tracking</h2>
+                            <Button>+ Website</Button>
+                        </div>
+                    : <div>
+
+                    </div>
+                }
             </div>
         </div>
     )
